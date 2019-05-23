53c8afdb77932543668eddb0712bbf51b71b6b25
idalovstrom <44798757+idalovstrom@users.noreply.github.com>
2019-05-23 16:36:51 +0200

    quiz

---
+++ quiz/script.js
@@ -0,0 +1,43 @@
+
+$(function(){
+	$('#sQ1').hide();
+	$('#sQ2').hide();
+	$('#sQ3').hide();
+	$('#ratta').hide();
+});
+$('.btn-success').click(function(){
+		$('#sQ1').show();
+		$('#sQ2').show();
+		$('#sQ3').show();
+	});
+
+
+var antalRatt = 0;
+const ratt1 = document.getElementById('rattNamn');
+
+ratt1.addEventListener('click', function() {
+antalRatt++;
+console.log(antalRatt);
+})
+
+
+const ratt2 = document.getElementById('rattAlder');
+
+ratt2.addEventListener('click', function() {
+antalRatt++;
+console.log(antalRatt);
+})
+
+
+const ratt3 = document.getElementById('rattLangd');
+
+ratt3.addEventListener('click', function() {
+antalRatt++;
+console.log(antalRatt);
+$('#ratta').show();
+})
+
+const ratta = document.getElementById('ratta');
+ratta.addEventListener('click', function() {
+	document.getElementById('rattSvar').innerHTML = 'Du fick ' +antalRatt+ ' av 3 rätt';
+})
